4d895e998d34cf6d9dd39cbf0299e600cd4d0196
Sid Ahuja <sidahuja@gmail.com>
2024-04-11 21:37:47 -0700

    added and debugged tests

--- R/calculate_model_metric.R
+++ R/calculate_model_metric.R
@@ -7,7 +7,6 @@
 #' @param truth_col A vector containing the ground truth values
 #' @param predictions_col A vector containing the prediction values
 #' @param metric The metric to calculate (e.g., "accuracy", "precision", "recall")
-#' @param event_level The event level for precision and recall calculations. Default is "first".
 #'
 #' @return The calculated value of the specified metric
 #'
@@ -15,27 +14,22 @@
 #'
 #' @examples
 #' df = yardstick::two_class_example
-#' calculate_model_metric(df, "truth", "predicted", "accuracy",
-#' event_level = "first")
+#' calculate_model_metric(df, "truth", "predicted", "accuracy")
 #'
 calculate_model_metric <- function(predictions,
                                    truth_col = "",
                                    predictions_col = "",
-                                   metric = "",
-                                   event_level = "first") {
+                                   metric = "") {
   if (metric == "accuracy") {
-    metric_table <- predictions |>
-    yardstick::metrics(truth = truth_col, estimate = predictions_col) |>
+    metric_table <- yardstick::metrics(predictions, truth = truth_col, estimate = predictions_col) |>
     dplyr::filter(.metric == "accuracy")
     metric_value <- metric_table$.estimate
   } else if (metric == "precision") {
-    metric_table <- predictions |>
-    yardstick::precision(truth = truth_col, estimate = predictions_col,
+    metric_table <- yardstick::precision(predictions, truth = truth_col, estimate = predictions_col,
                          event_level="first")
     metric_value <- metric_table$.estimate
   } else if (metric == "recall") {
-    metric_table <- predictions |>
-    yardstick::recall(truth = truth_col, estimate = predictions_col,
+    metric_table <- yardstick::recall(predictions, truth = truth_col, estimate = predictions_col,
                       event_level="first")
     metric_value <- metric_table$.estimate
   } else {
